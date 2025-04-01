86783dea67fc248b19994ca29565812b08495a38
Patryk Petryszen <p.petryszen@gmail.com>
2025-04-01 13:04:21 +0200

    25. Path Converters

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views #This imports the views.py file in the same directory together with function index there
 
 urlpatterns = [
-    path("january", views.january), #If the URL is /january, call the index function in views.py
-    path("february", views.february),
-    path("<month>", views.monthly_challenge), #This will call the monthly_challenge function in views.py and pass the month as an argument
+    path("<int:month>", views.monthly_challenge_by_number), #This will call the monthly_challenge_by_number function in views.py and pass the month as an integer argument
+    path("<str:month>", views.monthly_challenge), #This will call the monthly_challenge function in views.py and pass the month as an string argument, but /notmonth will also be in use
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!") #This is the response that will be returned when the URL is /challenges/january
-
-def february(request):
-    return HttpResponse("Walk for at least 30 minutes every day.") #This is the response that will be returned when the URL is /challenges/february
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
 def monthly_challenge(request, month):
     if month == "august":
