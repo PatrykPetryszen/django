5d290313fc68cd072277fbcb50b5b1c606077c10
p.petryszen <p.petryszen@ocado.com>
2025-04-01 10:25:52 +0200

    23. Adding More Views & URLs

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views #This imports the views.py file in the same directory together with function index there
 
 urlpatterns = [
-    path("january", views.index) #If the URL is /january, call the index function in views.py
+    path("january", views.january), #If the URL is /january, call the index function in views.py
+    path("february", views.february)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. This works!")
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!") #This is the response that will be returned when the URL is /challenges/january
+
+def february(request):
+    return HttpResponse("Walk for at least 30 minutes every day.") #This is the response that will be returned when the URL is /challenges/february
