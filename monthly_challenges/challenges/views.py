01de4793d0b2fd89ddf143982fe31a43232616ee
Patryk Petryszen <p.petryszen@gmail.com>
2025-04-01 13:25:34 +0200

    26. Adding More Dynamic View Logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "Get fit",
+    "february": "Get a job",
+    "march": "Get a girlfriend",
+    "april": "Get a car",
+    "may": "Get a house",
+    "june": "Get a life",
+    "july": "Get a dog",
+    "august": "Your birhday",
+    "september": "Eat no meat",
+    "october": "Learn Django",
+    "november": "Walk for at least 20 minutes",
+    "december": "Learn Django"
+}
 
 # Create your views here.
 
@@ -7,6 +22,8 @@ def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    if month == "august":
-        return HttpResponse("It's your birthday!")
-    return HttpResponse("The month is " + month) #This is the response that will be returned when the URL is /challenges/<month>
+    try:
+        text_to_month = monthly_challenges[month]
+        return HttpResponse(text_to_month)
+    except KeyError:
+        return HttpResponseNotFound("Please provide the name of the month")
